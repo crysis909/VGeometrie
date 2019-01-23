0258952ecdc7139f11dcae9b760b2495767aea1f
Markus <crysis909@users.noreply.github.com>
2019-01-23 17:34:45 +0100

    Adding functions
    Added Groupbox
    Added Exchange function (buggy)
    Added Select Line function
    Expaned UI

--- widget.cpp
+++ widget.cpp
@@ -3,6 +3,7 @@
 #include <QDebug>
 #include <QGraphicsTextItem>
 #include <algorithm>
+#include <QList>
 
 Widget::Widget(QWidget *parent) :
     QWidget(parent),
@@ -42,7 +43,7 @@ void Widget::createBtnGroupBox()
 
 void Widget::createSliderGroupBox()
 {
-    sliderGroupBox = new QGroupBox("Points");
+    sliderGroupBox = new QGroupBox("Generate");
     lblSlider = new QLabel("2");
     vSlider = new QSlider(Qt::Horizontal);
     QHBoxLayout *layout = new QHBoxLayout;
@@ -54,6 +55,40 @@ void Widget::createSliderGroupBox()
     layout->addWidget(vSlider);
 
     sliderGroupBox->setLayout(layout);
+}
+
+void Widget::createPointGroupBox()
+{
+    QVBoxLayout *pointLayout = new QVBoxLayout;
+    QHBoxLayout *firstLayer = new QHBoxLayout;
+    QHBoxLayout *secondLayer = new QHBoxLayout;
+    pointGroupBox = new QGroupBox("Point");
+    lblPoint = new QLabel("Point Nr.");
+    lblTextAngle = new QLabel("Angle: ");
+    lblAngle = new QLabel("0°");
+    spinPoint = new QSpinBox();
+    spinPoint1 = new QSpinBox();
+    spinPoint2 = new QSpinBox();
+    btnExchange = new QPushButton("Exchange");
+
+    spinPoint->setEnabled(false);
+    spinPoint1->setEnabled(false);
+    spinPoint2->setEnabled(false);
+    btnExchange->setEnabled(false);
+
+    firstLayer->addWidget(lblPoint);
+    firstLayer->addWidget(spinPoint);
+    firstLayer->addWidget(lblTextAngle);
+    firstLayer->addWidget(lblAngle);
+
+    secondLayer->addWidget(spinPoint1);
+    secondLayer->addWidget(spinPoint2);
+    secondLayer->addWidget(btnExchange);
+
+    pointLayout->addLayout(firstLayer);
+    pointLayout->addLayout(secondLayer);
+
+    pointGroupBox->setLayout(pointLayout);
 }
 
 void Widget::init()
@@ -69,18 +104,23 @@ void Widget::init()
     //GroupBox
     createBtnGroupBox();
     createSliderGroupBox();
+    createPointGroupBox();
 
     //Properties
     view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 
+    //Add Grougbox
     sideLayout->addWidget(btnGroupBox);
     sideLayout->addWidget(sliderGroupBox);
+    sideLayout->addWidget(pointGroupBox);
     sideLayout->setAlignment(Qt::AlignTop);
 
+    //Add Main Widgets
     mainLayout->addWidget(view);
     mainLayout->addLayout(sideLayout);
 
+    //Basic Properties
     setWindowTitle("Geometry");
     setLayout(mainLayout);
     resize(sizeHint());
@@ -90,7 +130,9 @@ void Widget::connects()
 {
     connect(btnGenerate, SIGNAL(pressed()), this, SLOT(onGenerate()));
     connect(btnOutline, SIGNAL(pressed()), this, SLOT(onOutline()));
+    connect(btnExchange, SIGNAL(pressed()), this, SLOT(onExchange()));
     connect(vSlider, SIGNAL(valueChanged(int)), lblSlider, SLOT(setNum(int)));
+    connect(spinPoint, SIGNAL(valueChanged(int)), this, SLOT(selectLine(int)));
 }
 
 void Widget::generateRandomPoints(int points)
@@ -126,9 +168,33 @@ void Widget::connectPoints()
 
 }
 
+bool Widget::sortByAngle()
+{
+    //Nähersten Punkt finden (0,0) -> Linie zu jedem Punkt und den Winkel messen
+    QPointF *topLeft = points[0];
+    for(QPointF *point : points)
+    {
+        if(point->rx() < topLeft->rx() && point->ry() < topLeft->ry())
+            topLeft = point;
+    }
+
+    for(int x = 0; x < points.size(); x++)
+    {
+        if(topLeft != points[x])
+        {
+            QLineF *line = new QLineF(*topLeft, *points[x]);
+
+            qDebug() << line->angle();
+
+            delete line;
+        }
+    }
+}
+
 void Widget::createPoly()
 {
     polygon.clear();
+    sortByAngle();
     std::sort(points.begin(), points.end()); //Dummy function
 
     for(QPointF *point : points)
@@ -137,6 +203,7 @@ void Widget::createPoly()
 
 void Widget::draw()
 {
+    scene->clear();
     drawPoints();
     drawLines();
 }
@@ -148,7 +215,7 @@ void Widget::drawPoints()
         int i = 0;
         for(QPointF *point : points)
         {
-            scene->addEllipse(point->rx() - diameter / 2, point->ry() - diameter / 2, diameter, diameter);
+            pointsItem.append(scene->addEllipse(point->rx() - diameter / 2, point->ry() - diameter / 2, diameter, diameter));
             QGraphicsTextItem *text = new QGraphicsTextItem(QString::number(i + 1));
             text->setPos(*point);
             text->setScale(1.5);
@@ -162,7 +229,7 @@ void Widget::drawLines()
 {
     if(!lines.empty())
         for(QLineF *line : lines)
-            scene->addLine(*line);
+            linesItem.append(scene->addLine(*line));
 }
 
 void Widget::drawPoly()
@@ -194,17 +261,38 @@ void Widget::clearVectors()
 
     for(auto poly : polygon)
         delete &poly;
+
+    for(auto point : pointsItem)
+        delete point;
+    pointsItem.clear();
+
+    for(auto line : linesItem)
+        delete line;
+    linesItem.clear();
+
     polygon.clear();
 }
 
 void Widget::onGenerate()
 {
-    scene->clear();
     clearVectors();
 
     generateRandomPoints(vSlider->value());
     connectPoints();
     draw();
+
+    btnExchange->setEnabled(true);
+    spinPoint->setEnabled(true);
+    spinPoint1->setEnabled(true);
+    spinPoint2->setEnabled(true);
+
+    spinPoint->setMinimum(1);
+    spinPoint->setMaximum(lines.length());
+
+    spinPoint1->setMinimum(1);
+    spinPoint1->setMinimum(1);
+    spinPoint1->setMaximum(points.length());
+    spinPoint2->setMaximum(points.length());
 }
 
 void Widget::onOutline()
@@ -214,3 +302,34 @@ void Widget::onOutline()
     createPoly();
     drawPoly();
 }
+
+void Widget::onExchange()
+{
+
+    QPointF *p1 = points[spinPoint1->value() - 1];
+    QPointF *p2 = points[spinPoint2->value() - 1];
+
+    points[spinPoint2->value() - 1] = p1;
+    points[spinPoint1->value() - 1] = p2;
+
+    draw();
+    //selectLine(spinPoint->value());
+}
+
+void Widget::selectLine(int value)
+{
+    QGraphicsLineItem *lineItem = linesItem[value - 1];
+
+    if(value - 1 > 0)
+    {
+        QGraphicsLineItem *prev = linesItem[value -  2];
+        prev->setPen(QPen(Qt::black));
+    }
+
+    QString angle = QString::number(lineItem->line().angle());
+    lblAngle->setText(angle + "°");
+
+    QPen pen(Qt::red, 5, Qt::DashLine, Qt::RoundCap, Qt::RoundJoin);
+    lineItem->setPen(pen);
+}
+
--- widget.h
+++ widget.h
@@ -13,6 +13,7 @@
 #include <QPushButton>
 #include <QSlider>
 #include <QLabel>
+#include <QSpinBox>
 
 
 namespace Ui {
@@ -28,14 +29,19 @@ public:
     ~Widget();
 
     //Functions
+    bool sortByAngle();
+
 protected slots:
     void onGenerate();
     void onOutline();
+    void onExchange();
+    void selectLine(int value);
 
 private:
     void init();
     void createBtnGroupBox();
     void createSliderGroupBox();
+    void createPointGroupBox();
 
     void draw();
     void drawPoints();
@@ -43,6 +49,7 @@ private:
     void drawPoly();
     void generateRandomPoints(int points);
     void createPoly();
+
     void connectPoints();
     void connects();
 
@@ -54,24 +61,49 @@ private:
 private:
     //UI
     Ui::Widget *ui;
+
+    //Layouts
     QHBoxLayout *mainLayout;
     QVBoxLayout *sideLayout;
+
+    //Groupbox
     QGroupBox *btnGroupBox;
     QGroupBox *sliderGroupBox;
+    QGroupBox *pointGroupBox;
 
     QGraphicsScene *scene;
     QGraphicsView *view;
-    QPushButton *btnGenerate;
-    QPushButton *btnOutline;
+
+    //PointBox
+    //First Layer
+    QLabel *lblPoint;
+    QLabel *lblTextAngle;
+    QLabel *lblAngle;
+    QSpinBox *spinPoint;
+
+    //Second Layer
+    QPushButton *btnExchange;
+    QSpinBox *spinPoint1;
+    QSpinBox *spinPoint2;
+
+    //SliderBox
     QLabel *lblSlider;
     QSlider *vSlider;
+
+    //ButtonBox
+    QPushButton *btnGenerate;
+    QPushButton *btnOutline;
+
 
     //Other
     QVector<QPointF*> points;
     QVector<QLineF*> lines;
+    QVector<QGraphicsEllipseItem*> pointsItem;
+    QVector<QGraphicsLineItem*> linesItem;
     QPolygonF polygon;
 
     const int diameter = 4;
 };
+
 
 #endif // WIDGET_H
